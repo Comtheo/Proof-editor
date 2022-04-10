c670675110f5f1ea60ea8b4a9a8232d786ddc7d2
Samuel Vavrek <samuel.vavrek@gmail.com>
2022-04-10 19:52:57 +0200

    moznost pridavat jednoduche spojenia medzi pohybovatelnymi prvkami

--- script.js
+++ script.js
@@ -4,14 +4,20 @@
     .controller('excontroller', ['$window', '$scope', '$sce', function($window, $scope, $sce) {
       
       $scope.steps = [
-        {"Id":10,
-        "Description":"Description",
-        "Note":"Note",
+        {"Description":"",
+        "Note":"",
         "VerticalPos":"100px",
-        "HorizontalPos":"50",
+        "HorizontalPos":"50px",
         "SRC":"<p>Hello world!</p>"}
       ]
-      var dragged = 0;
+
+      $scope.connections = [
+        {
+        }
+      ]
+
+
+      var dragged = 0, connecting = false;
       $scope.move = function(event) 
       {
         if(dragged!=-1)
@@ -21,6 +27,21 @@
             var y = event.clientY;
             $scope.steps[dragged].VerticalPos = (y-75)+"px";
             $scope.steps[dragged].HorizontalPos = (x-100)+"px";
+            var other_x, other_y;
+            for (let i=0;i<$scope.connections.length;i++){
+              if(dragged == $scope.connections[i].id1)
+              {
+                $scope.connections[i].x1=x;
+                $scope.connections[i].y1=y;
+                
+              }
+              if(dragged == $scope.connections[i].id2)
+              {
+                $scope.connections[i].x2=x;
+                $scope.connections[i].y2=y;
+              }
+                      
+            }
         }
       };
 
@@ -29,7 +50,8 @@
       };
 
       $scope.setDraggable = function(index){
-        dragged = index;
+        if(!connecting)
+          dragged = index;
       }
       var edited = -1;
       $scope.editStep = function(index){
@@ -47,9 +69,39 @@
         generator = latexjs.parse(text, { generator: generator });
         $scope.steps[edited].SRC = generator.htmlDocument("https://cdn.jsdelivr.net/npm/latex.js@0.12.4/dist/").documentElement.outerHTML;
         document.getElementById("frame-"+edited).srcdoc = $scope.steps[edited].SRC;
-        console.log($scope.steps[edited].SRC);
         document.getElementById("dial").style.display = "none";
         edited = -1;
+      }
+
+      $scope.startConnecting = function(){
+        window.alert("Click on an element to connect to");
+        connecting = true;
+      }
+
+      $scope.Connect = function(index){
+        if((index==edited)&&connecting)
+        {
+          window.alert("Can't connect an element with itself")
+        }
+        else
+        if(connecting)
+        {
+          var x1 = parseInt($scope.steps[edited].HorizontalPos,10)+100
+          var x2 =parseInt($scope.steps[index].HorizontalPos,10)+100
+          var y1 =parseInt($scope.steps[edited].VerticalPos,10)+75
+          var y2 =parseInt($scope.steps[index].VerticalPos,10)+75
+        $scope.connections.push({
+          "id1":edited,
+          "id2":index,
+          "x1":x1,
+          "x2":x2,
+          "y1":y1,
+          "y2":y2
+        })
+        connecting = false;
+        window.alert("Connected");
+      }
+        
       }
 
       $scope.unsetDraggable = function(){
@@ -67,11 +119,5 @@
         }
         )
       }
-
-
-
-      $scope.back = function(){
-        //document.getElementById("dial").style.display = "initial";
-      };
     }]);
   })(window.angular);
